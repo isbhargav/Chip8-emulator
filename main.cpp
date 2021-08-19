a0c476a0eabac947ff581d375009b4990e2b91c4
Bhargav <ladbhargav@yahoo.co.in>
2021-08-19 16:01:56 -0400

    Add info about Chip 8
    Add information about Chip 8 memory, registers and instruction set and other details that we will be required to refer back to when implementing the emulator

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 /*
+ * Link to Chip 8 refrence : http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
 Chip 8 Memory Map (total memory 4kb):
 +---------------+= 0xFFF (4095) End of Chip-8 RAM
 |               |
@@ -27,10 +28,18 @@ Chip 8 Memory Map (total memory 4kb):
 +---------------+= 0x000 (0) Start of Chip-8 RAM
 
  Registers:
- There are total 16 (8 bit) registers named as V0,V1,...Vf
- There is also special 16 bit I register which stores address lower 12 bits(0x000-0x1ff)
- Vf register is not used by program, it used as flag register
+    -  There are total 16 (8 bit) registers named as V0,V1,...Vf
+    -  There is also special 16 bit I register which stores address lower 12 bits(0x000-0x1ff)
+    -  Vf register is not used by program, it used as flag register
+    - Program Counter (PC) should be 16 bit (points to address of next instruction to be executed)
+    - Stack pointer should be 8 bit (points to top of the stack)
+    - Stack is an array of 16 16bit value, address to return to after the subroutine has finished
 
+Instructions
+    - Chip-8 language includes 36 different instructions
+    - All instructions are 2 bytes long and are stored most-significant-byte first
+    - In memory, the first byte of each instruction should be located at an even addresses.
+    - If a program includes sprite data, it should be padded so any instructions following it will be properly situated in RAM.
 
  */
 
